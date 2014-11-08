d3344c59a21b278797b255ba1c6014dc230e6075
Brian Knaus <briank.lists@gmail.com>
2014-11-07 19:27:17 -0800

    Create poly_gt.cpp

---
+++ poly_gt.cpp
@@ -0,0 +1,50 @@
+
+
+/* Homozygotes. */
+/* A1=1.0 */
+
+/* Heterozygotes */
+/* A1=0.5, A2=0.5 */
+
+/* Bi-allelic triploids */
+/* A1=2/3, A2=1/3 */
+
+/* Tri-allelic triploids */
+/* A1=1/3, A2=1/3, A3=1/3 */
+
+/* Bi-allelic tetraploids */
+/* A1=0.75, A2=0.25 */
+
+/* Tri-allelic tetraploids */
+/* A1=0.5, A2=0.25, A3=0.25 */
+
+/* Tetra-allelic tetraploids */
+/* A1=0.25, A2=0.25, A3=0.25, A4=0.25 */
+
+/* Bi-allelic pentaploids */
+/* A1=4/5, A2=1/5 */
+
+/* Bi-allelic pentaploids */
+/* A1-3/5, A2=1/5, A3=1/5 */
+/* A1-2/5, A2=2/5, A3=1/5 */
+
+/* Tri-allelic pentaploids */
+/* A1=3/5, A2=1/5, A3=1/5 */
+/* A1=2/5, A2=2/5, A3=1/5 */
+
+/* Tetra-allelic pentaploids */
+/* A1=2/5, A2=1/5, A3=1/5, A4=1/5 */
+
+/* Bi-allelic hexaploid */
+/* A1=5/6, A2=1/6 */
+
+/* Tri-allelic hexaploid */
+/* A1=4/6, A2=1/6, A3=1/6 */
+/* A1=3/6, A2=2/6, A3=1/6 */
+
+/* Tetra-allelic hexaploid */
+/* A1=3/6, A2=1/6, A3=1/6, A4=1/6 */
+/* A1=2/6, A2=2/6, A3=1/6, A4=1/6 */
+
+
+
