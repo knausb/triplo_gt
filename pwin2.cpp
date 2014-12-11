b2152b956e697c09dc6d5c37a71e3f1976a93e44
knausb <briank.lists@gmail.com>
2014-12-11 14:26:29 -0800

    updated pwin2.cpp

--- pwin2.cpp
+++ pwin2.cpp
@@ -31,6 +31,48 @@ void init_win_sums(int nsamp, int nGTs[], int nNAs[],
 }
 
 
+void proc_sample(int RD, int GT, int &nGTs, int &nNAs, int &RDs, int nPDs[7], string data){
+  vector <string> fields;
+  boost::algorithm::split( fields, data, boost::algorithm::is_any_of( ":" ) );
+
+  RDs = RDs + stoi(fields[RD]);
+
+}
+
+
+
+void proc_win(int nsamp, int nGTs[], int nNAs[], int RDs[],
+              int nPDs[][7], int MPs[],  
+              std::vector <string> lines){
+
+  vector <string> data;
+  vector <string> fields;
+//  cout << lines[0] << "\n";
+
+  for(int i=0; i<lines.size(); i++){
+    boost::algorithm::split( data, lines[i], boost::algorithm::is_any_of( "\t" ) );
+    boost::algorithm::split( fields, data[8], boost::algorithm::is_any_of( ":" ) );
+//    cout << data[8] << "\n";
+
+    /* Determine format positions */
+    int RD = 0, GT = 0;
+//    int CT = 0;
+    for(int i=0; i<fields.size(); i++){
+      if(fields[i] == "RD"){RD = i;}
+//      if(fields[i] == "CT"){CT = i;}
+      if(fields[i] == "GT"){GT = i;}
+    }
+
+
+    for(int j=9; j<data.size(); j++){
+//      cout << fields[j] << "\t";
+      proc_sample(RD, GT, nGTs[j-9], nNAs[j-9], RDs[j-9], nPDs[j-9], data[j]);
+    }
+//  cout << "\n";
+  }
+}
+
+
 
 /* ----- ----- ***** ----- ----- */
 /*        Print functions        */
@@ -84,13 +126,6 @@ int main(int argc, char **argv) {
   vector <string> fields;
   int opt, win = 999;
   int nsamp;
-  int nGTs[nsamp];
-  int nNAs[nsamp];
-  int  RDs[nsamp];
-  int nPDs[nsamp][7];
-  int  MPs[nsamp];
-
-  init_win_sums(nsamp, nGTs, nNAs, RDs, nPDs, MPs);
 
 
   /* Parse command line options. */
@@ -110,39 +145,70 @@ int main(int argc, char **argv) {
   }
 
 
+  /* Read in the first line. */
+  getline (cin,line);
+
+  /* In case there is no header */
+  if(line[0] != '#'){
+    cerr << "Error, this does not appear to be a properly formatted file:\n";
+    cerr << "Expecting a header beginning with '#'.\n";
+    cerr << line << "\n\n";
+    exit(EXIT_FAILURE);
+  }
+
+
+  while(line[0] == '#' & line[1] == '#'){
+    /* Omit meta lines which begin with '##'. */
+    getline (cin,line);
+//    cout << line << "\n\n";
+  }
+//  cout << line << "\n\n";
+
+  if (line[0] == '#' & line[1] == 'C'){
+    /* Manage header line which begins with '#CHROM'. */
+    boost::algorithm::split( fields, line, boost::algorithm::is_any_of( "\t" ) );
+    nsamp = fields.size() - 9;  // Determine the number of samples.
+    for(int i=9; i<fields.size(); i++){
+      snames.push_back(fields[i]);
+    }
+  } else {
+    cerr << "Poorly formed file\n";
+    cerr << "Expecting a header line begining with '#CHROM'\n";
+    cerr << line;
+    cerr << "\n";
+    exit(EXIT_FAILURE);
+  }
+
+
+  /* Process records. */
+  int start = 1;
+  int stop = start + win;
+  int last = start;
+
+  int nGTs[nsamp];
+  int nNAs[nsamp];
+  int  RDs[nsamp];
+  int nPDs[nsamp][7];
+  int  MPs[nsamp];
+
+  init_win_sums(nsamp, nGTs, nNAs, RDs, nPDs, MPs);
+
+  print_header(snames);
+
+
   while(getline (cin,line) ){
-
-    if(line[0] == '#' & line[1] == '#'){
-      /* Omit meta lines which begin with '##'. */
-    } else if (line[0] == '#' & line[1] == 'C'){
-      /* Manage header line which begins with '#CHROM'. */
-      boost::algorithm::split( fields, line, boost::algorithm::is_any_of( "\t" ) );
-      nsamp = fields.size() - 9;  // Determine the number of samples.
-      for(int i=9; i<fields.size(); i++){
-        snames.push_back(fields[i]);
-      }
+    boost::algorithm::split( fields, line, boost::algorithm::is_any_of( "\t" ) );
+    if(atoi(fields[1].c_str()) > stop){
+      /* Begin a new window. */
+      proc_win(nsamp, nGTs, nNAs, RDs, nPDs, MPs, lines);
+//      print_win();
+      init_win_sums(nsamp, nGTs, nNAs, RDs, nPDs, MPs);
+      start = stop + 1;
+      stop = start + win;
+      lines.clear();
+      lines.push_back(line);
     } else {
-
-      /* Process records. */
-      print_header(snames);
-      int start = 1;
-      int stop = start + win;
-      int last = start;
-
-
-
-
-      if(atoi(fields[1].c_str()) > stop){
-        /* If there are no records in the window. */
-        //print_null(fields[0], start, stop, snames);
-        start = stop + 1;
-        stop = start + win;
-        lines.clear();
-        lines.push_back(line);
-        init_win_sums(nsamp, nGTs, nNAs, RDs, nPDs, MPs);
-      } else {
-        lines.push_back(line);
-      }
+      lines.push_back(line);
     }
   }
 
